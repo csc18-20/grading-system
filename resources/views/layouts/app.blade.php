c2d28548abc9c0f535907152e3acb3fed2bf72d8
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2018-01-28 14:24:21 +0300

    First commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Grading System</title>
+    <link rel="stylesheet" href="/public/css/app.css">
+</head>
+<body>
+    @include('layouts.navBar')
+    @yield("content")
+    <!-- footer -->
+</body>
+</html>
